caf81df697232da876fdc7f7f3c1c9ea036817b1
alexandre-dev29 <axel.business29@gmail.com>
2022-05-04 15:53:27 +0200

    Configure all the project with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: { primaryDark: "#393D46" },
+    },
+  },
+  plugins: [],
+};
