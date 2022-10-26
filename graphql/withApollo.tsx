f2c1688d2fa6e566e690398facb1fe0058404677
alexandre-dev29 <axel.business29@gmail.com>
2022-10-26 17:47:52 +0200

    resolving on launch error

--- graphql/withApollo.tsx
+++ graphql/withApollo.tsx
@@ -6,13 +6,11 @@ import { ApolloProvider } from "@apollo/client";
 import { getApolloClient } from "./graphqlConfig";
 import { Toaster } from "react-hot-toast";
 import { useGraphqlErrorState } from "../states";
-import ErrorPopup from "../components/ErrorPopup";
 import { DarkTheme, LightTheme } from "../config";
 
 // eslint-disable-next-line react/display-name
 export const withApollo = (Comp: NextPage) => (props: any) => {
-  const { isOpen, setIsOpen, errorType, messagesError } =
-    useGraphqlErrorState();
+  const { isOpen, setIsOpen, errorType, messagesError } = useGraphqlErrorState();
 
   function closeModal() {
     setIsOpen(false);
@@ -20,12 +18,12 @@ export const withApollo = (Comp: NextPage) => (props: any) => {
 
   return (
     <div style={{ position: "relative", zIndex: 0 }}>
-      <ErrorPopup
-        errorType={errorType}
-        messages={messagesError}
-        onCloseEvent={closeModal}
-        openStatus={isOpen}
-      />
+      {/*<ErrorPopup*/}
+      {/*  errorType={errorType}*/}
+      {/*  messages={messagesError}*/}
+      {/*  onCloseEvent={closeModal}*/}
+      {/*  openStatus={isOpen}*/}
+      {/*/>*/}
       <Head>
         <title>Welcome to NextStrap ECommerce</title>
       </Head>
@@ -35,9 +33,7 @@ export const withApollo = (Comp: NextPage) => (props: any) => {
         value={{ light: LightTheme.className, dark: DarkTheme.className }}
       >
         <NextUIProvider>
-          <ApolloProvider
-            client={getApolloClient(undefined, props.apolloState)}
-          >
+          <ApolloProvider client={getApolloClient(undefined, props.apolloState)}>
             <Toaster />
             <Comp />
           </ApolloProvider>
